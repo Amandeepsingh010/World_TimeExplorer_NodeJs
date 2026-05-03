29c71dcc31c8e1215cbb8d1ab210dec01a4adbd9
Amandeepsingh010 <sainiamandeep010@gmail.com>
2026-05-03 15:24:30 +0530

    data.js file

---
+++ data.js
@@ -0,0 +1,58 @@
+export const countries = [
+  {
+    name: "India",
+    flag: "🇮🇳",
+    zones: [{ city: "New Delhi", tz: "Asia/Kolkata" }]
+  },
+  {
+    name: "Canada",
+    flag: "🇨🇦",
+    zones: [
+      { city: "Toronto", tz: "America/Toronto" },
+      { city: "Vancouver", tz: "America/Vancouver" }
+    ]
+  },
+  {
+    name: "USA",
+    flag: "🇺🇸",
+    zones: [
+      { city: "New York", tz: "America/New_York" },
+      { city: "Los Angeles", tz: "America/Los_Angeles" }
+    ]
+  },
+  {
+    name: "UK",
+    flag: "🇬🇧",
+    zones: [{ city: "London", tz: "Europe/London" }]
+  },
+  {
+    name: "UAE",
+    flag: "🇦🇪",
+    zones: [{ city: "Dubai", tz: "Asia/Dubai" }]
+  },
+  {
+    name: "Australia",
+    flag: "🇦🇺",
+    zones: [{ city: "Sydney", tz: "Australia/Sydney" }]
+  },
+  {
+    name: "Germany",
+    flag: "🇩🇪",
+    zones: [{ city: "Berlin", tz: "Europe/Berlin" }]
+  },
+  {
+    name: "Japan",
+    flag: "🇯🇵",
+    zones: [{ city: "Tokyo", tz: "Asia/Tokyo" }]
+  },
+  {
+    name: "Brazil",
+    flag: "🇧🇷",
+    zones: [{ city: "São Paulo", tz: "America/Sao_Paulo" }]
+  },
+  {
+    name: "South Africa",
+    flag: "🇿🇦",
+    zones: [{ city: "Cape Town", tz: "Africa/Johannesburg" }]
+  }
+];
